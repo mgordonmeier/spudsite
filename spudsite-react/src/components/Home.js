88ed20b4f0405fdc4a4c6a6dd5469fea577bebd7
Max Meier <funknspudzilla@MacBook-Pro.local>
2022-11-09 16:26:16 -0600

    Font work and alignment, tried background

--- spudsite-react/src/components/Home.js
+++ spudsite-react/src/components/Home.js
@@ -1,16 +1,26 @@
 import React from "react";
 import './Home.css';
 
+
 function Home() {
 
     return (
-        <div className="container">
-            <div className="text-center">
-                <div className="rocksalt" style={{ display: "inline-block" }}>
-                    <h1 className="m-4">Welcome to the Spudsite</h1>
-                    <h3 className="m-2">Home of</h3>
-                    <div className="frame-2">
-                        <img src="/img/FreshSpudsCover.png" alt="Fresh Spuds Cover" className="img-header" />
+        <div style={{ backgroundImage: '../img/pour4.JPG' }}>
+            <div className="container">
+                <div className="text-center">
+                    <div className="rocksalt" style={{ display: "inline-block" }}>
+                        <div className="card mt-2 container specialCard" style={{ width: "62%" }}>
+                            <h1 className="m-4">Welcome to the Spudsite</h1>
+                            <h3 className="mb-4">Home of</h3>
+                        </div>
+                        <div className="frame-2">
+                            <img src="/img/FreshSpudsCover.png" alt="Fresh Spuds Cover" className="img-header" />
+                        </div>
+                        <div className="card mb-3 container specialCard" style={{ width: "90%", display: "inline-block" }}>
+                            <p className="m-2">Funk N Spuds is a Minneapolis based musical collective of artists from around the Midwest.</p>
+                            <p className="m-2">Formed in early 2020, their jam-rock jazz-pop sounds inspire hope in listeners and positive change in the world.</p>
+                            {/* <p className="m-2">Funk N Spuds has taken the stage at most venues in the Twin Cities, but also </p> */}
+                        </div>
                     </div>
                 </div>
             </div>
--- spudsite-react/src/components/NavBar.js
+++ spudsite-react/src/components/NavBar.js
@@ -5,9 +5,9 @@ function NavBar() {
 
     return (
         <div className="container">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between" style={{ borderRadius: "20px" }}>
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">Home</Link>
+                    <Link className="navbar-brand" to="/"><img href="spudsite-react/public/img/FnSLogo.svg" alt="Fresh Spuds Logo" style={{width: "3%"}}/></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
