3cdeff407efb401655621c6ba67d683802914b85
Max Meier <funknspudzilla@MacBook-Pro.local>
2022-11-11 17:13:27 -0600

    song symbols

--- spudsite-react/src/components/Home.js
+++ spudsite-react/src/components/Home.js
@@ -1,8 +1,10 @@
 import React from "react";
 import './Home.css';
-import background from "../img/pour4.JPG"
-import jumpingSpud from "../img/FunkNJump.gif"
-import spudCover from "../img/FreshSpudsCover.png"
+import background from "../img/pour4.JPG";
+import jumpingSpud from "../img/FunkNJump.gif";
+import spudCover from "../img/FreshSpudsCover.png";
+import envelope from "../img/EnvelopeSongSymbol.png";
+import lhc from "../img/LHCSongSymbol.png";
 
 
 function Home() {
@@ -16,15 +18,21 @@ function Home() {
                             <h1 className="m-4 spudsite">Welcome to the Spudsite</h1>
                             <h3 className="mb-4">Home of</h3>
                         </div>
-
+                        <div className="text-start">
+                            <img src={envelope} alt="Envelope Song Symbol" className="songSymbol" />
+                        </div>
                         <div className="funk-n-jump">
-                            <img style={{height: "10em", width: "15em"}} src={jumpingSpud} alt="Jumping Spud" />                        </div>
+                            <img style={{ height: "10em", width: "15em" }} src={jumpingSpud} alt="Jumping Spud" />
+                        </div>
                         <div className="frame-2">
                             <img src={spudCover} alt="Fresh Spuds Cover" className="img-header" />
                         </div>
-                        <div className="card mb-3 container specialCard" style={{ width: "90%", display: "inline-block" }}>
-                            <p className="m-2">Funk N Spuds is a Minneapolis based musical collective of artists from around the Midwest.</p>
-                            <p className="m-2">Formed in early 2020, their jam-rock jazz-pop sounds inspire hope in listeners and positive change in the world.</p>
+                        <div className="text-end me-10">
+                            <img src={lhc} alt="Low Hanging Ceilings Song Symbol" className="songSymbol" />
+                        </div>
+                        <div className="card mb-3 container specialCard beenie" style={{ width: "75%", display: "inline-block" }}>
+                            <p className="m-1">Funk N Spuds is a Minneapolis based musical collective of artists from around the Midwest.</p>
+                            <p className="m-1">Formed in early 2020, their jam-rock jazz-pop sounds inspire hope in listeners and positive change in the world.</p>
                             {/* <p className="m-2">Funk N Spuds has taken the stage at most venues in the Twin Cities, but also </p> */}
                         </div>
                     </div>
--- spudsite-react/src/index.js
+++ spudsite-react/src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import background from "./img/pour4.JPG";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div style={{
+    backgroundImage: background,
+    backgroundSize: "cover",
+    height: 1300
+  }}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </div>
 );
